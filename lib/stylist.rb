d12e1f0625a24c0480133131417267316ec752e1
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-13 19:39:06 +0300

    shows all the stylists save in the database.

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,7 +1,20 @@
 class Stylist
   attr_reader(:name, :id)
 
+ # initialize method.
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
+
+# fetches all the names of the stylist's available.
+ define_singleton_method(:all) do
+    returned_movies = DB.exec("SELECT * FROM stylists;")
+    stylists = []
+    returned_stylists.each() do |stylist|
+      name = stylist.fetch("name")
+      id = stylist.fetch("id").to_i()
+      stylists.push(Stylist.new({:name => name, :id => id}))
+    end
+    stylists
+ end
