85318ec3185d5160d34bd1735414f1648cfe5202
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-14 12:48:27 +0300

    added the all method to fetch data.

--- lib/client.rb
+++ lib/client.rb
@@ -6,3 +6,15 @@ class Client
     @name = attributes.fetch(:name)
     @id =attributes.fetch(:id)
   end
+
+#loops fetching all the clients in the database.
+define_singleton_method(:all) do
+  returned_clients = DB.exec("SELECT * FROM clients;")
+  clients = []
+  returned_clients.each() do |client|
+    name = client.fetch("name")
+    id = client.fetch("id").to_i()
+    clients.push(Client.new({:name => name, :id => id}))
+end
+clients
+end
