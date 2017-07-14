2b7f34d09b6e75acc9276077d793d554ab919946
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-14 13:05:03 +0300

    compares to datas method

--- lib/client.rb
+++ lib/client.rb
@@ -31,3 +31,8 @@ define_method(:save) do
   result = DB.exec("INSERT INTO clients (name) VALUES ('#{@name}') RETURNING id;")
   @id = result.first().fetch("id").to_i()
 end
+
+#compare method.
+define_method(:==) do |another_client|
+  self.name().==(another_client.name()).&(self.id().==(another_client.id()))
+end
