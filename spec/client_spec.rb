ca80f8763b73abeab1fd939c51d6274c08dd3aae
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-14 12:04:31 +0300

    added the initialize method

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,4 +1,15 @@
 require('spec_helper')
 
 describe(Client) do
+  # initialize method for the client.
+  define_method(:initialize) do
+    it('is initialized with a name') do
+      client = Client.new(nmae: 'Lucy', id: nil)
+      expect(client).to(be_an_instance_of(Client))
+    end
+    it('can be initialized with its database ID') do
+      client = Client.new(name: 'Lucy', id: 1)
+      expect(client).to(be_an_instance_of(Client))
+    end
+  end
 end
