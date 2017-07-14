d7247967408c4a6a2778a4d59bf4fce7bae34971
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-14 13:11:03 +0300

    added the delete function.

--- lib/client.rb
+++ lib/client.rb
@@ -1,45 +1,51 @@
 class Client
   attr_reader(:name, :id)
 
-#initialize method.
+  # initialize method.
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
-    @id =attributes.fetch(:id)
+    @id = attributes.fetch(:id)
   end
 
-#loops fetching all the clients in the database.
-define_singleton_method(:all) do
-  returned_clients = DB.exec("SELECT * FROM clients;")
-  clients = []
-  returned_clients.each() do |client|
-    name = client.fetch("name")
-    id = client.fetch("id").to_i()
-    clients.push(Client.new({:name => name, :id => id}))
-end
-clients
-end
+  # loops fetching all the clients in the database.
+  define_singleton_method(:all) do
+    returned_clients = DB.exec('SELECT * FROM clients;')
+    clients = []
+    returned_clients.each do |client|
+      name = client.fetch('name')
+      id = client.fetch('id').to_i
+      clients.push(Client.new(name: name, id: id))
+    end
+    clients
+  end
 
-#find data through the database.
-define_singleton_method(:find) do |id|
-  result = DB.exec("SELECT * FROM clients WHERE id = #{id};")
-  name = result.first().fetch("name")
-  Client.new({:name =>name, :id => id})
-end
+  # find data through the database.
+  define_singleton_method(:find) do |id|
+    result = DB.exec("SELECT * FROM clients WHERE id = #{id};")
+    name = result.first.fetch('name')
+    Client.new(name: name, id: id)
+  end
 
-#save method.
-define_method(:save) do
-  result = DB.exec("INSERT INTO clients (name) VALUES ('#{@name}') RETURNING id;")
-  @id = result.first().fetch("id").to_i()
-end
+  # save method.
+  define_method(:save) do
+    result = DB.exec("INSERT INTO clients (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first.fetch('id').to_i
+  end
 
-#compare method.
-define_method(:==) do |another_client|
-  self.name().==(another_client.name()).&(self.id().==(another_client.id()))
-end
+  # compare method.
+  define_method(:==) do |another_client|
+    name.==(another_client.name).&(id.==(another_client.id))
+  end
 
-#update method.
-define_method(:update) do |attributes|
-  @name = attributes.fetch(:name, @name)
-  @id = self.id()
-  DB.exec("UPDATE clients SET name = '#{@name}' WHERE id = #{@id};")
+  # update method.
+  define_method(:update) do |attributes|
+    @name = attributes.fetch(:name, @name)
+    @id = id
+    DB.exec("UPDATE clients SET name = '#{@name}' WHERE id = #{@id};")
+  end
+
+  # delete function.
+  define_method(:delete) do
+    DB.exec("DELETE FROM clients WHERE id = #{id};")
+  end
 end
