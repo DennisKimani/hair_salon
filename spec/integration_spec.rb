70d4e4154055a6b43518602f53b35fcad1cfebc6
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-15 12:45:28 +0300

    adding clients to a stylist.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -15,7 +15,7 @@ end
 
 describe('viewing all of the stylists', type: :feature) do
   it('allows a user to see all of the stylists that have been created') do
-    stylist = Stylist.new(name: 'Moringaschool Homework')
+    stylist = Stylist.new(name: 'Moringa')
     stylist.save
     visit('/')
     click_link('View All Stylists')
@@ -25,12 +25,23 @@ end
 
 describe('seeing details for a single stylist', type: :feature) do
   it('allows a user to click a stylist to see the clients and details for it') do
-    test_stylist = Stylist.new(name: 'School stuff')
+    test_stylist = Stylist.new(name: 'School')
     test_stylist.save
-    test_client = Client.new(description: 'learn SQL', stylist_id: test_stylist.id)
+    test_client = Client.new(name: 'Fiona', stylist_id: test_stylist.id)
     test_client.save
     visit('/stylists')
     click_link(test_stylist.name)
-    expect(page).to have_content(test_client.description)
+    expect(page).to have_content(test_client.name)
   end
 end
+
+describe('adding clients to a stylist', {:type => :feature}) do
+    it('allows a user to add a client to a stylist') do
+      test_stylist = Stylist.new({:name => 'Rose'})
+      test_stylist.save()
+      visit("/stylists/#{test_stylist.id()}")
+      fill_in("Name", {:with => "John"})
+      click_button("Add client")
+      expect(page).to have_content("Success")
+    end
+  end
