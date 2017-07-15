b1a7f354f27bd38ded0d2bc356e0142f8f0bb8ae
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-15 11:59:09 +0300

    a user can select a single stylist to see the clients

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,7 +4,7 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('adding a new stylist', type: :feature) do
-  it('allows a user to click a stylist to see the task and details for it') do
+  it('allows a user to click a stylist to see the client and details for it') do
     visit('/')
     click_link('Add a new Stylist')
     fill_in('name', with: 'Jane')
@@ -20,5 +20,17 @@ describe('viewing all of the stylists', type: :feature) do
     visit('/')
     click_link('View All Stylists')
     expect(page).to have_content(stylist.name)
+  end
+end
+
+describe('seeing details for a single stylist', type: :feature) do
+  it('allows a user to click a stylist to see the clients and details for it') do
+    test_stylist = Stylist.new(name: 'School stuff')
+    test_stylist.save
+    test_client = Client.new(description: 'learn SQL', stylist_id: test_stylist.id)
+    test_client.save
+    visit('/stylists')
+    click_link(test_stylist.name)
+    expect(page).to have_content(test_client.description)
   end
 end
