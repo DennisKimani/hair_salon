532600323df7360379a61f233306fe667bbfa7f7
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-15 11:04:55 +0300

    linked the index page

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/client')
+require('./lib/stylist')
 require('pg')
 
 DB = PG.connect(dbname: 'hair_salon')
+
+get("/") do
+  erb(:index)
+end
